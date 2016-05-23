6d368bea2f66da98b7460244ca1621a756cec92c
khremlajn <arek.zaluski@gmail.com>
2016-05-23 14:30:57 +0100

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -10,8 +10,79 @@ JQ="jq --raw-output --exit-status"
 deploy_image() {
     autorization_token=$(aws ecr get-authorization-token --registry-ids 792082350620 --output text --query authorizationData[].authorizationToken | base64 --decode | cut -d: -f2)
     docker login -u AWS -p $autorization_token -e none https://792082350620.dkr.ecr.us-west-2.amazonaws.com
-    docker tag webapp-repository:latest 792082350620.dkr.ecr.us-west-2.amazonaws.com/webapp-repository:latest
-    docker push 792082350620.dkr.ecr.us-west-2.amazonaws.com/webapp-repository
+    docker tag webapp-repository:$CIRCLE_SHA1 792082350620.dkr.ecr.us-west-2.amazonaws.com/webapp-repository:$CIRCLE_SHA1
+    docker push 792082350620.dkr.ecr.us-west-2.amazonaws.com/webapp-repository:$CIRCLE_SHA1
+}
+
+# reads $CIRCLE_SHA1, $host_port
+# sets $task_def
+make_task_def() {
+
+    task_template='[
+	{
+	    "name": "start",
+	    "image": "792082350620.dkr.ecr.us-west-2.amazonaws.com/webapp-repository:%s",
+	    "essential": true,
+	    "memory": 200,
+	    "cpu": 10,
+	    "command": [
+	       "python app.py"
+	    ],
+	    "portMappings": [
+            {
+                "containerPort": 5000,
+                "hostPort": 80
+            }
+        ]
+	}
+    ]'
+
+    task_def=$(printf "$task_template" $CIRCLE_SHA1 $host_port)
+
+}
+
+# reads $family
+# sets $revision
+register_definition() {
+
+    if revision=$(aws ecs register-task-definition --container-definitions "$task_def" --family $family | $JQ '.taskDefinition.taskDefinitionArn'); then
+        echo "Revision: $revision"
+    else
+        echo "Failed to register task definition"
+        return 1
+    fi
+
+}
+
+deploy_cluster() {
+
+    host_port=80
+    family="webapp-cluster"
+
+    make_task_def
+    register_definition
+    if [[ $(aws ecs update-service --cluster webapp-cluster --service webapp-ecs-service --task-definition $revision | \
+                   $JQ '.service.taskDefinition') != $revision ]]; then
+        echo "Error updating service."
+        return 1
+    fi
+
+    # wait for older revisions to disappear
+    # not really necessary, but nice for demos
+    for attempt in {1..30}; do
+        if stale=$(aws ecs describe-services --cluster webapp-cluster --services webapp-ecs-service | \
+                       $JQ ".services[0].deployments | .[] | select(.taskDefinition != \"$revision\") | .taskDefinition"); then
+            echo "Waiting for stale deployments:"
+            echo "$stale"
+            sleep 5
+        else
+            echo "Deployed!"
+            return 0
+        fi
+    done
+    echo "Service update took too long."
+    return 1
 }
 
 deploy_image
+deploy_cluster
