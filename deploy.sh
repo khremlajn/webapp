664166e0b5ab75ddb4fb6c0a3d269221b9a100d8
khremlajn <arek.zaluski@gmail.com>
2016-05-23 16:17:49 +0100

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -10,8 +10,8 @@ JQ="jq --raw-output --exit-status"
 deploy_image() {
     autorization_token=$(aws ecr get-authorization-token --registry-ids 792082350620 --output text --query authorizationData[].authorizationToken | base64 --decode | cut -d: -f2)
     docker login -u AWS -p $autorization_token -e none https://792082350620.dkr.ecr.us-west-2.amazonaws.com
-    docker tag webapp-repository:$CIRCLE_SHA1 792082350620.dkr.ecr.us-west-2.amazonaws.com/webapp-repository:$CIRCLE_SHA1
-    docker push 792082350620.dkr.ecr.us-west-2.amazonaws.com/webapp-repository:$CIRCLE_SHA1
+    docker tag webapp-repository:$CIRCLE_SHA1 792082350620.dkr.ecr.us-west-2.amazonaws.com/webapp-repository:latest
+    docker push 792082350620.dkr.ecr.us-west-2.amazonaws.com/webapp-repository:latest
 }
 
 # reads $CIRCLE_SHA1, $host_port
@@ -52,7 +52,7 @@ deploy_cluster() {
 
     make_task_def
     register_definition
-    if [[ $(aws ecs update-service --cluster webapp-cluster --service webapp-ecs-service --task-definition $revision | \
+    if [[ $(aws ecs update-service --cluster webapp-cluster --service webaspp-service2 --task-definition $revision | \
                    $JQ '.service.taskDefinition') != $revision ]]; then
         echo "Error updating service."
         return 1
@@ -76,4 +76,4 @@ deploy_cluster() {
 }
 
 deploy_image
-deploy_cluster
+#deploy_cluster
